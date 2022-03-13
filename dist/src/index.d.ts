0e11958757c7f532fda2f7eb5f3fac0d5542677c
ChrisLuo <313963196@qq.com>
2022-03-14 01:05:08 +0800

    delete dist folder

--- dist/src/index.d.ts
+++
@@ -1,5 +0,0 @@
-import { getFirstDayAndEndDayOfWeek, getFirstDayAndEndDayOfMonth, isInRange } from "./range/range";
-import { isSameOrAfter, isSameOrBefore } from "./tool/tool";
-import { Dayjs } from "dayjs";
-export declare type DateParam = string | Date | Dayjs;
-export { getFirstDayAndEndDayOfWeek, getFirstDayAndEndDayOfMonth, isInRange, isSameOrBefore, isSameOrAfter, };
